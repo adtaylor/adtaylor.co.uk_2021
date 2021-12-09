2be0dd35fc215e263e18a05d64f2c1347cae623a
Ad Taylor <ad@thebeansgroup.com>
2021-12-09 22:53:20 +0000

    Fixes Linting errors

--- pages/index.js
+++ pages/index.js
@@ -29,8 +29,8 @@ export default function Home() {
       <Container>
         <h2>A mildly narcasistic page about me.</h2>
 
-        <p>I'm a Head of Developement, former JS lead and hobby collector.</p>
-        <p>I've build this little site to house future projects and playgrounds.</p>
+        <p>{"I'm a Head of Developement, former JS lead and hobby collector."}</p>
+        <p>{"I've build this little site to house future projects and playgrounds."}</p>
 
         <p><TextLink css={extLink} href="" title="A link to Ad Taylors LinkedIn profile">LinkedIn</TextLink></p>
         <p><TextLink css={extLink} href="https://github.com/adtaylor" title="A link to Ad Taylors Github profile">Github</TextLink></p>
