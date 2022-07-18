e174c6c61b348c8e59df2b0aea5480824d5492ed
Ad Taylor <ad@thebeansgroup.com>
2022-07-18 21:25:32 +0100

    Rebuild amplify app

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -5,13 +5,5 @@ export type AmplifyDependentResourcesAttributes = {
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
-    },
-    "function": {
-        "spotifyPlaylistFetch": {
-            "Name": "string",
-            "Arn": "string",
-            "Region": "string",
-            "LambdaExecutionRole": "string"
-        }
     }
 }
