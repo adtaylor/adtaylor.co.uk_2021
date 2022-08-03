b6f3371766baf667abeceae26f0af4a2cb3d3ff8
Ad Taylor <ad@thebeansgroup.com>
2022-08-03 18:29:41 +0100

    Remove i18n for next export support

--- next.config.js
+++ next.config.js
@@ -11,9 +11,5 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
   // Append the default value with md extensions
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-  reactStrictMode: true,
-  i18n: {
-    locales: ['en-GB'],
-    defaultLocale: 'en-GB',
-  }
+  reactStrictMode: true
 })
